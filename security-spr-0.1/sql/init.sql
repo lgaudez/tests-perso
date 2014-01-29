ac491368e9de16669f3199fb5f9dc4bc3774f3f3
lgaudez <leo.gaudez@gmail.com>
2014-01-29 18:30:27 +0100

    ajout du repertoire sql

---
+++ security-spr-0.1/sql/init.sql
@@ -0,0 +1,8 @@
+INSERT INTO monapp.roles (id,role) VALUES (1,'admin'), (2,'moderator'), (3,'user');
+
+INSERT INTO monapp.users (id,login, password, firstName, lastName, mail,citizenShip, spokenLanguages, dateOfBirth) VALUES
+  (1,'leo', '123456', 'léo', 'Gaudez', 'leo.gaudez@toncul.com', 'français', 'anglais; français', '05/04/1989' ),
+  (2,'admin', '123456','vfdv', 'vfd', 'leo.vfd@toncul.com', 'français', 'anglais; français', '05/04/1989' ),
+  (3,'utilisateur1', '123456', 'vfdv', 'vfd', 'leo.vfd@toncul.com', 'français', 'anglais; français', '05/04/1989');
+
+INSERT INTO monapp.user_roles (user_id, role_id) VALUES (1, 2), (2, 1), (3,3);
---
+++ security-spr-0.1/sql/tablecreations.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `roles` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL UNIQUE ,
+  `password` varchar(20) NOT NULL,
+  `firstName` varchar(30) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `mail` varchar(50) NOT NULL UNIQUE,
+  `citizenShip` varchar(50) NOT NULL,
+  `spokenLanguages` varchar(250)  NOT NULL,
+  `dateOfBirth` varchar(10)  NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_roles` (
+  `user_id` int(6) NOT NULL,
+  `role_id` int(6) NOT NULL,
+  KEY `user` (`user_id`),
+  KEY `role` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
